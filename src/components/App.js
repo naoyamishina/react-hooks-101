e4626a220cd21170ab0522fdeb94d2d26a05f2f2
mishina naoya <msnyxx811@gmail.com>
2024-01-03 15:03:12 +0900

    イベント一覧の表示

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useReducer, useState } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import Event from './Event'
 import reducer from '../reducers'
 
 const App = () => {
@@ -21,8 +22,6 @@ const App = () => {
     setTitle('')
     setBody('')
   }
-
-  console.log(state)
 
   return (
     <div className="container-fluid">
@@ -53,6 +52,7 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
+          { state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
         </tbody>
       </table>
     </div>
